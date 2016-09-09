962b21f1e9cd030154b85486032ccba0627d9d2f
Alok Kumar <alokkumar003@gmail.com>
2016-09-09 17:46:18 +0530

    All Code done, login with FB account, Display the details. Fbauth is used. added two Framework FBSDKCoreKit,and FBSDKLoginKit

--- Task_IOS/ViewController.swift
+++ Task_IOS/ViewController.swift
@@ -13,19 +13,28 @@ class ViewController: UIViewController, FBSDKLoginButtonDelegate{
     @IBOutlet weak var profileimage: UIImageView!
     @IBOutlet weak var btnFB: FBSDKLoginButton!
     @IBOutlet weak var username: UILabel!
+    @IBOutlet weak var username2: UILabel!
+    @IBOutlet weak var email: UILabel!
+   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        facebookPage()
     }
     
     func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
-        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large), email,age,timezone"]).startWithCompletionHandler { (connection, result, error) -> Void in
             
             let strFirstName: String = (result.objectForKey("first_name") as? String)!
             let strLastName: String = (result.objectForKey("last_name") as? String)!
+            let stremail: String = (result.objectForKey("email") as? String)!
             let strPictureURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
             
-            self.username.text = "Welcome, \(strFirstName) \(strLastName)"
+            self.username.text = " \(strFirstName) "
+            self.username2.text = "\(strLastName)"
+            self.email.text = "\(stremail)"
             self.profileimage.image = UIImage(data: NSData(contentsOfURL: NSURL(string: strPictureURL)!)!)
         }
 
