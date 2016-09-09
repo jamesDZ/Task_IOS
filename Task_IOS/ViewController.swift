2c8bd7848a16a50446cbe110c8f04ebd26755907
Alok Kumar <alokkumar003@gmail.com>
2016-09-09 15:43:12 +0530

    Framework and Delegate method of FB.

--- Task_IOS/ViewController.swift
+++ Task_IOS/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate{
 
+    @IBOutlet weak var profileimage: UIImageView!
+    @IBOutlet weak var btnFB: FBSDKLoginButton!
+    @IBOutlet weak var username: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        FBSDKGraphRequest.init(graphPath: "me", parameters: ["fields":"first_name, last_name, picture.type(large)"]).startWithCompletionHandler { (connection, result, error) -> Void in
+            
+            let strFirstName: String = (result.objectForKey("first_name") as? String)!
+            let strLastName: String = (result.objectForKey("last_name") as? String)!
+            let strPictureURL: String = (result.objectForKey("picture")?.objectForKey("data")?.objectForKey("url") as? String)!
+            
+            self.username.text = "Welcome, \(strFirstName) \(strLastName)"
+            self.profileimage.image = UIImage(data: NSData(contentsOfURL: NSURL(string: strPictureURL)!)!)
+        }
 
+    }
+
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!)
+    {
+        let loginManager: FBSDKLoginManager = FBSDKLoginManager()
+        loginManager.logOut()
+        
+        profileimage.image = nil
+        username.text = ""
+    }
+
+    func facebookPage()
+    {
+        btnFB.readPermissions = ["public_profile", "email", "user_friends"];
+        btnFB.delegate = self
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
